b4bb5f531f18899c4151280284088aa4d264eb12
Nimphina <rune80@hotmail.co.uk>
2013-04-20 22:49:59 +0100

    dev-1.0.9

--- IRC/Main.cs
+++ IRC/Main.cs
@@ -11,9 +11,9 @@ namespace IRC
         public static IrcClient irc = new IrcClient();
         public string server = "irc.esper.net";
         private int port = 6667;
-        private string channel = "#nimphina";
+        private string rootchannel = "#botspam";
         public const string botop = "Nimphina";
-        public const string version = "dev-1.0.5";
+        public const string version = "dev-1.0.9";
         public string botname = "Hershey";
 
         public static void Main()
@@ -21,34 +21,34 @@ namespace IRC
 
             Console.Title = "Nimbot terminal";
 
-          /*  try
-            {
-                TextReader reader = new StreamReader("config");
-                reader.ReadLine();
-                string getsetting = reader.ReadLine();
-                Console.WriteLine(getsetting);
-                reader.Close();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                Console.WriteLine("Creating a blank config file");
-                StreamWriter writer = new StreamWriter("config");
-                writer.WriteLine("Server:");
-                writer.WriteLine(" ");
-                writer.WriteLine("Port:");
-                writer.WriteLine(" ");
-                writer.WriteLine("Bot nick:");
-                writer.WriteLine(" ");
-                writer.WriteLine("Botop:");
-                writer.WriteLine(" ");
-                writer.WriteLine("Command char:");
-                writer.WriteLine(" ");
-                writer.Close();
-                Console.WriteLine("Now exiting");
-                Console.ReadLine();
-                Environment.Exit(0);
-            } */
+            /*  try
+              {
+                  TextReader reader = new StreamReader("config");
+                  reader.ReadLine();
+                  string getsetting = reader.ReadLine();
+                  Console.WriteLine(getsetting);
+                  reader.Close();
+              }
+              catch (Exception e)
+              {
+                  Console.WriteLine(e.Message);
+                  Console.WriteLine("Creating a blank config file");
+                  StreamWriter writer = new StreamWriter("config");
+                  writer.WriteLine("Server:");
+                  writer.WriteLine(" ");
+                  writer.WriteLine("Port:");
+                  writer.WriteLine(" ");
+                  writer.WriteLine("Bot nick:");
+                  writer.WriteLine(" ");
+                  writer.WriteLine("Botop:");
+                  writer.WriteLine(" ");
+                  writer.WriteLine("Command char:");
+                  writer.WriteLine(" ");
+                  writer.Close();
+                  Console.WriteLine("Now exiting");
+                  Console.ReadLine();
+                  Environment.Exit(0);
+              } */
 
             ClientDemo demo = new ClientDemo();
         }
@@ -75,57 +75,68 @@ namespace IRC
         void OnConnecting(object sender, EventArgs e)
         {
             Console.WriteLine("Starting Nimbot version: {0} ", version);
-            Console.WriteLine("Botop: {0}, Command char: {1},", botop, "~");
+            Console.WriteLine("Botop: {0}, Command char: {1},", botop, "#");
             Console.WriteLine(" ");
             Console.WriteLine("Connecting to server {0} on port {1}.", server, port);
         }
 
         void OnConnected(object sender, EventArgs e)
         {
+            if (rootchannel == "fofftopic")
+            {
+                Environment.Exit(0);
+            }
             Console.WriteLine("Connected to {0}.", server);
             //irc.SendMessage(SendType.Message, channel, "Joined: " + channel + " Bot op is: " + botop, Priority.BelowMedium);
 
             irc.Login(botname, botname, 0, botname);
-            irc.RfcJoin(channel);
-            Console.WriteLine("Joining {0}.", channel);
+            irc.RfcJoin(rootchannel);
+            Console.WriteLine("Joining {0}.", rootchannel);
             irc.Listen(true);
         }
 
-        public void OnChannelMessage (object sender, IrcEventArgs e)
-		{
-			Console.WriteLine (e.Data.Type + ":");
-			Console.WriteLine ("(" + e.Data.Channel + ") <" + e.Data.Nick + "> " + e.Data.Message);
-			string opsymbol = "#";
+        public void OnChannelMessage(object sender, IrcEventArgs e)
+        {
+            Console.WriteLine(e.Data.Type + ":");
+            Console.WriteLine("(" + e.Data.Channel + ") <" + e.Data.Nick + "> " + e.Data.Message);
+            string opsymbol = "#";
+            string channel = e.Data.Channel;
 
-			string message = e.Data.Message;
-			string nick = e.Data.Nick;
+            string message = e.Data.Message;
+            string nick = e.Data.Nick;
 
-			if (e.Data.Message.StartsWith (opsymbol)) {
-				message = message.Trim (new Char[] { '#' });
-				bcommands.bc (botop, channel, nick, message, irc);
-			}
+            if (e.Data.Message.StartsWith(opsymbol))
+            {
+                message = message.Trim(new Char[] { '#' });
+                bcommands.bc(botop, channel, nick, message, irc);
+            }
 
-			if (e.Data.Nick == "Ralph") {
-				irc.SendMessage (SendType.Message, channel, "I hate Ralph and he hates me", Priority.High);
-				Console.WriteLine ("RALPH SAID SHIT");
-			}
+            if (e.Data.Nick == "Ralph")
+            {
+                irc.SendMessage(SendType.Message, channel, "I hate Ralph and he hates me", Priority.High);
+                Console.WriteLine("RALPH SAID SHIT");
+            }
 
-			if (e.Data.Message == "What is love?") {
+            if (e.Data.Message == "What is love?")
+            {
 
-				irc.SendMessage (SendType.Message, channel, "Baby don't hurt me", Priority.High);
-			}
-			if (e.Data.Message == "Hodor!") {
+                irc.SendMessage(SendType.Message, channel, "Baby don't hurt me", Priority.High);
+            }
+            if (e.Data.Message == "Hodor!")
+            {
 
-				irc.SendMessage (SendType.Message, channel, "Oh shut up", Priority.High);
-			}
-			if (e.Data.Message == "The war z") {
+                irc.SendMessage(SendType.Message, channel, "Oh shut up", Priority.High);
+            }
+            if (e.Data.Message == "The war z")
+            {
 
-				irc.SendMessage (SendType.Message, channel, "http://www.youtube.com/watch?v=RtKAm3nzg6I", Priority.High);
-			}
-			if (e.Data.Message == "Hello" || e.Data.Message == "hello" || e.Data.Message == "Hi" || e.Data.Message == "hi") {
-			
-				irc.SendMessage (SendType.Message, channel, string.Format("Hello {0}", e.Data.Nick) , Priority.High);
-			}
+                irc.SendMessage(SendType.Message, channel, "http://www.youtube.com/watch?v=RtKAm3nzg6I", Priority.High);
+            }
+            if (e.Data.Message == "Hello" || e.Data.Message == "hello" || e.Data.Message == "Hi" || e.Data.Message == "hi")
+            {
+
+                irc.SendMessage(SendType.Message, channel, string.Format("Hello {0}", e.Data.Nick), Priority.High);
+            }
         }
 
         void OnDisconnected(object sender, EventArgs e)
--- IRC/bcommands.cs
+++ IRC/bcommands.cs
@@ -9,8 +9,9 @@ namespace IRC
     class bcommands
     {
 
-        public static void bc (string botop, string channel, string nick, string message, IrcClient irc){
-           
+        public static void bc(string botop, string channel, string nick, string message, IrcClient irc)
+        {
+
             string[] args = message.TrimEnd().Split(' ');
             Console.WriteLine("A command was used!");
             int lnth = args.Length;
@@ -20,7 +21,7 @@ namespace IRC
             {
                 case "u":
                     Console.WriteLine("U command");
-               
+
                     if (lnth == 2)
                     {
                         irc.SendMessage(SendType.Message, channel, string.Format("{0}: http://u.mcf.li/{1}", nick, args[1]));
@@ -33,26 +34,27 @@ namespace IRC
                     {
                         irc.SendMessage(SendType.Message, channel, string.Format("{0}, List of options: [profile, posts, topics, warnings, videos, friends, pm, names, admin, edit, modcp, validate, warn, suspend, iphistory]", nick));
                     }
-                    break; 
+                    break;
 
-				case "reddit":
-				if (lnth == 2)
+                case "reddit":
+                    if (lnth == 2)
                     {
                         irc.SendMessage(SendType.Message, channel, string.Format("{0}: http://reddit.com/r/{1}", nick, args[1]));
                     }
-				else{
-					irc.SendMessage(SendType.Message, channel, string.Format("{0}: http://reddit.com/r/{1}", nick, "{1}"));
-					}
-				break;
+                    else
+                    {
+                        irc.SendMessage(SendType.Message, channel, string.Format("{0}: http://reddit.com/r/{1}", nick, "{1}"));
+                    }
+                    break;
 
                 case "Ping":
                 case "ping":
                     Console.WriteLine("ping command");
-                    irc.SendMessage(SendType.Message, channel, "pong", Priority.High);
+                    irc.SendMessage(SendType.Message, string.Format(channel, "#botspam"), "pong", Priority.High);
                     break;
 
                 case "info":
-                    irc.SendMessage(SendType.Message, channel, "I am a shitty little bot created by Nimphina using the smartirc4net lib", Priority.High);
+                    irc.SendMessage(SendType.Message, channel, "I am a bot written by Nimphina, very messily written using the SmartIrc4net lib for irc stuff. Sauce code: http://github.com/Nimphina/Nimbot", Priority.High);
                     break;
 
                 case "quit":
@@ -74,7 +76,185 @@ namespace IRC
                 case "hi":
                     irc.SendMessage(SendType.Message, channel, "Hello, " + nick, Priority.High);
                     break;
-                
+
+                case "join":
+                    if (args[1] != "#fofftopic")
+                    {
+                        irc.RfcJoin(args[1]);
+                    }
+                    else
+                    {
+                        irc.SendMessage(SendType.Message, channel, "Joining #fofftopic is not allowed!", Priority.High);
+                    }
+                    break;
+
+                case "part":
+                    irc.RfcPart(args[1]);
+                    break;
+
+                case "add":
+                    try
+                    {
+                        double[] operands = new double[lnth];
+                        double noresult = 0;
+                        if (lnth >= 3)
+                        {
+                            for (int i = 1; i < lnth; i++)
+                            {
+                                double workingnum = double.Parse(args[i]);
+                                operands[i] = workingnum;
+                                Console.WriteLine(operands[i]);
+
+                                noresult = noresult + operands[i];
+                            }
+
+                            string result = Convert.ToString(noresult);
+                            irc.SendMessage(SendType.Message, channel, result, Priority.High);
+
+
+                            if (result == "69")
+                            {
+                                irc.SendMessage(SendType.Message, channel, "Heh heh, 69, hehe", Priority.High);
+                            }
+                        }
+                        else
+                        {
+                            irc.SendMessage(SendType.Message, channel, "placholder message", Priority.High);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        irc.SendMessage(SendType.Message, channel, e.Message, Priority.High);
+                    }
+                    break;
+
+                case "minus":
+                    //Note, fix 1 - 1 producing -1 when you can be bothered
+                    try
+                    {
+                        double[] operands = new double[lnth];
+                        double noresult = 1;
+                        if (lnth >= 3)
+                        {
+                            for (int i = 1; i < lnth; i++)
+                            {
+                                double workingnum = double.Parse(args[i]);
+                                operands[i] = workingnum;
+                                Console.WriteLine(operands[i]);
+
+                                noresult = noresult - operands[i];
+                            }
+
+                            string result = Convert.ToString(noresult);
+                            irc.SendMessage(SendType.Message, channel, result, Priority.High);
+
+
+                            if (result == "69")
+                            {
+                                irc.SendMessage(SendType.Message, channel, "Heh heh, 69, hehe", Priority.High);
+                            }
+                        }
+                        else
+                        {
+                            irc.SendMessage(SendType.Message, channel, "placholder message", Priority.High);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        irc.SendMessage(SendType.Message, channel, e.Message, Priority.High);
+                    }
+                    break;
+
+                case "multiply":
+                    try
+                    {
+                        double[] operands = new double[lnth];
+                        double noresult = 1;
+                        if (lnth >= 3)
+                        {
+                            for (int i = 1; i < lnth; i++)
+                            {
+                                double workingnum = double.Parse(args[i]);
+                                operands[i] = workingnum;
+                                Console.WriteLine(operands[i]);
+
+                                noresult = noresult * operands[i];
+                            }
+
+                            string result = Convert.ToString(noresult);
+                            irc.SendMessage(SendType.Message, channel, result, Priority.High);
+
+
+                            if (result == "69")
+                            {
+                                irc.SendMessage(SendType.Message, channel, "Heh heh, 69, hehe", Priority.High);
+                            }
+                        }
+                        else
+                        {
+                            irc.SendMessage(SendType.Message, channel, "placholder message", Priority.High);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        irc.SendMessage(SendType.Message, channel, e.Message, Priority.High);
+                    }
+                    break;
+
+                case "divide":
+                    try
+                    {
+                        double[] operands = new double[lnth];
+                        double noresult = 1;
+                        if (lnth >= 3)
+                        {
+                            for (int i = 1; i < lnth; i++)
+                            {
+                                double workingnum = double.Parse(args[i]);
+                                operands[i] = workingnum;
+                                Console.WriteLine(operands[i]);
+
+                                noresult = noresult / operands[i];
+                            }
+
+                            string result = Convert.ToString(noresult);
+                            irc.SendMessage(SendType.Message, channel, result, Priority.High);
+
+
+                            if (result == "69")
+                            {
+                                irc.SendMessage(SendType.Message, channel, "Heh heh, 69, hehe", Priority.High);
+                            }
+                        }
+                        else
+                        {
+                            irc.SendMessage(SendType.Message, channel, "placholder message", Priority.High);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        irc.SendMessage(SendType.Message, channel, e.Message, Priority.High);
+                    }
+                    break;
+
+                case "condebug":
+                    if (nick == botop)
+                    {
+                        irc.SendMessage(SendType.Message, channel, string.Format("{0}, Messges from console enabled", botop), Priority.High);
+                        while (true)
+                        {
+                            Console.WriteLine("Enter a message: ");
+                            string consolemessage = Console.ReadLine();
+
+                            if (consolemessage == "/stop")
+                            {
+                                irc.SendMessage(SendType.Message, channel, string.Format("{0}, Messges from console disabled", botop), Priority.High);
+                                break;
+                            }
+                            irc.SendMessage(SendType.Message, channel, string.Format("[{0}]", consolemessage), Priority.High);
+                        }
+                    }
+                    break;
             }
         }
     }
