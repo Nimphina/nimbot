7b59999e399a91150ef2ce8af711381cd05a42f4
Daniel <rune80@hotmail.co.uk>
2013-04-22 11:22:33 +0200

    Updating messages

--- IRC/commands/subtraction.cs
+++ IRC/commands/subtraction.cs
@@ -42,7 +42,7 @@ namespace IRC
 				} 
 				else 
 				{
-					irc.SendMessage (SendType.Message, channel, "placholder message", Priority.High);
+					irc.SendMessage (SendType.Message, channel, "Needs some operands to work with!", Priority.High);
 				}
 			} 
 			catch (Exception e) 
